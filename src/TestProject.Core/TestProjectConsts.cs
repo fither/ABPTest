67b6d6f25962a0db9ca244987ad7517303816fe4
fither <onur.hamarattt@gmail.com>
2021-08-27 08:51:49 +0300

    initial commit

---
+++ src/TestProject.Core/TestProjectConsts.cs
@@ -0,0 +1,16 @@
+﻿namespace TestProject
+{
+    public class TestProjectConsts
+    {
+        public const string LocalizationSourceName = "TestProject";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+        
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public const string DefaultPassPhrase = "db7142a0f6284961bb79e9f691eccf78";
+    }
+}
---
+++ src/TestProject.EntityFrameworkCore/EntityFrameworkCore/TestProjectDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using TestProject.Authorization.Roles;
+using TestProject.Authorization.Users;
+using TestProject.MultiTenancy;
+using TestProject.Products;
+
+namespace TestProject.EntityFrameworkCore
+{
+    public class TestProjectDbContext : AbpZeroDbContext<Tenant, Role, User, TestProjectDbContext>
+    {
+        public DbSet<Product> Products { get; set; }
+        /* Define a DbSet for each entity of the application */
+        
+        public TestProjectDbContext(DbContextOptions<TestProjectDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
